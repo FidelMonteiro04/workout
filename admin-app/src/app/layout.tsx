f88c05d12bdadb7c3687fba2ee6d1838abf4243a
Felipe Souza <felipesouza036@gmail.com>
2023-05-10 12:57:42 -0300

    Creating Admin App front-end

---
+++ admin-app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Workout | Parceiro",
+  description:
+    "Nossa plataforma para parceiros, cadastre sua academia ou loja e comece de forma fácil e rápida!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
