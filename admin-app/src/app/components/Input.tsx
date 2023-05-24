31500da0ec5884f72f4886c2ad10a5ca70154f74
Felipe Souza <felipesouza036@gmail.com>
2023-05-24 13:17:28 -0300

    feat: Error field on input component

--- admin-app/src/app/components/Input.tsx
+++ admin-app/src/app/components/Input.tsx
@@ -12,6 +12,7 @@ interface Props {
   password?: boolean;
   registerField?: any;
   visibility?: boolean;
+  error?: string;
 }
 
 const Input = ({
@@ -19,33 +20,37 @@ const Input = ({
   registerField,
   password,
   visibility,
+  error,
   icon: Icon,
 }: Props) => {
   const [focus, setFocus] = useState(false);
   const [visible, setVisible] = useState(false);
   return (
-    <div
-      className={`flex transition w-full max-w-[280px] items-center px-3 py-2 border-[1px]  rounded-sm shadow-md ${
-        focus ? "border-primary-500 shadow-lg" : "border-gray-500"
-      }`}
-    >
-      {Icon && <Icon size={20} className="mr-2 text-primary-500  " />}
-      <input
-        {...registerField}
-        placeholder={placeholder}
-        type={password && !visible ? "password" : "text"}
-        className="w-full bg-transparent text-zinc-500 placeholder:text-zinc-400 text-lg outline-transparent font-light"
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-      />
-      {visibility && (
-        <div
-          onClick={() => setVisible((prev) => !prev)}
-          className="text-zinc-600 transition hover:text-primary-500 cursor-pointer"
-        >
-          {!visible ? <VisibleIcon size={20} /> : <InvisibleIcon size={20} />}
-        </div>
-      )}
+    <div className="w-full max-w-[280px] flex flex-col">
+      <div
+        className={`flex transition items-center px-3 py-2 border-[1px]  rounded-sm shadow-md ${
+          focus ? "border-primary-500 shadow-lg" : "border-gray-500"
+        } ${error ? "border-red-600" : ""}`}
+      >
+        {Icon && <Icon size={20} className="mr-2 text-primary-500  " />}
+        <input
+          {...registerField}
+          placeholder={placeholder}
+          type={password && !visible ? "password" : "text"}
+          className="w-full bg-transparent text-zinc-500 placeholder:text-zinc-400 text-lg outline-transparent font-light"
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+        />
+        {visibility && (
+          <div
+            onClick={() => setVisible((prev) => !prev)}
+            className="text-zinc-600 transition hover:text-primary-500 cursor-pointer"
+          >
+            {!visible ? <VisibleIcon size={20} /> : <InvisibleIcon size={20} />}
+          </div>
+        )}
+      </div>
+      <small className="text-red-600 text-xs">{error}</small>
     </div>
   );
 };
