974a652f38edf73e7fd4e5b5c4b056bf76c45afa
Felipe Souza <felipesouza036@gmail.com>
2023-05-26 13:48:56 -0300

    feat: Preview image

--- admin-app/src/app/components/AddImage.tsx
+++ admin-app/src/app/components/AddImage.tsx
@@ -1,20 +1,72 @@
 "use client";
 
-import { AiOutlinePlus } from "react-icons/ai";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
-const AddImage = () => {
+import { AiOutlinePlus } from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
+
+interface Props {
+  registerField?: any;
+  error?: string;
+}
+
+const AddImage = ({ registerField, error }: Props) => {
+  const [hasImage, setHasImage] = useState(false);
+  const imageRef = useRef({} as HTMLImageElement);
+  const onAddImage = (event: any) => {
+    console.dir(imageRef);
+    console.log("arquivo: ");
+    console.dir(event.target);
+    setHasImage(true);
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (!imageRef.current || !reader.result) return;
+      imageRef.current.src = reader.result.toString();
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
   return (
-    <div className="max-w-[240px] max-h-[240px] h-full">
-      <label
-        htmlFor="inputFile"
-        className="cursor-pointer hover:border-primary-600 hover:text-primary-600 transition min-w-[120px] min-h-[120px] w-full h-full flex flex-col items-center justify-center rounded-md border-[1px] border-primary-500 text-primary-500 p-6 hover:shadow-md hover:-translate-y-2 duration-200"
-      >
-        <AiOutlinePlus size={32} />
-        <span className="font-light group-hover:bg-primary-600 text-lg text-primary-500">
-          Imagem
-        </span>
-      </label>
-      <input accept=".jpg, .png, .jpeg" hidden id="inputFile" type="file" />
+    <div
+      className={`relative h-full w-full max-h-[240px] group ${
+        !hasImage
+          ? "max-w-[240px]"
+          : "max-w-[300px] lg:max-w-full flex justify-center items-center overflow-hidden rounded-md"
+      }`}
+    >
+      <img
+        src=""
+        ref={imageRef}
+        className="transition group-hover:brightness-50 w-full h-auto"
+        hidden={!hasImage}
+      />
+      {hasImage && (
+        <label
+          htmlFor="inputFile"
+          className="font-semibold cursor-pointer opacity-0 transition absolute left-1/2 flex top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:opacity-100 gap-1 hover:text-primary-500 text-white text-sm p-2"
+        >
+          <AiFillEdit size={20} />
+          <span>Editar</span>
+        </label>
+      )}
+      {!hasImage && (
+        <label
+          htmlFor="inputFile"
+          className="cursor-pointer hover:border-primary-600 hover:text-primary-600 transition min-w-[120px] min-h-[120px] w-full h-full flex flex-col items-center justify-center rounded-md border-[1px] border-primary-500 text-primary-500 p-6 hover:shadow-md hover:-translate-y-2 duration-200"
+        >
+          <AiOutlinePlus size={32} />
+          <span className="font-light text-lg text-primary-500">Imagem</span>
+        </label>
+      )}
+      <input
+        {...registerField}
+        accept=".jpg, .png, .jpeg"
+        hidden
+        id="inputFile"
+        type="file"
+        onChange={(event) => onAddImage(event)}
+      />
+      <small className="text-red-600">{error}</small>
     </div>
   );
 };
--- admin-app/src/app/register-place/page.tsx
+++ admin-app/src/app/register-place/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useForm } from "react-hook-form";
 
 import AddImage from "../components/AddImage";
 import Button from "../components/Button";
@@ -15,6 +16,13 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { BsCheck } from "react-icons/bs";
 
 const RegisterPlace = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    clearErrors,
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
   return (
     <>
       <h2 className="text-2xl lg:text-3xl text-secondary-500 max-w-[240px] lg:max-w-full font-semibold mb-6 lg:mb-0">
@@ -22,20 +30,59 @@ const RegisterPlace = () => {
       </h2>
       <div className="flex flex-col mb-4">
         <div className="max-w-[600px] mx-auto w-full h-full flex flex-col items-center">
-          <AddImage />
+          <AddImage
+            registerField={{ ...register("image", { required: true }) }}
+            error={errors.image && "É necessário ter uma imagem!"}
+          />
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <form className="flex flex-col gap-3 pt-4">
-              <Input placeholder="Nome" icon={BsBuildings} />
-              <Input placeholder="Número de personais" icon={PersonalIcon} />
-              <Input placeholder="Endereço" icon={AddressIcon} />
-              <Input placeholder="Link do Instagram" icon={BsInstagram} />
-              <Input placeholder="Contato" icon={ContactIcon} />
+            <div className="flex flex-col gap-3 pt-4">
+              <Input
+                registerField={{ ...register("placeName", { required: true }) }}
+                error={errors.placeName && "O nome do local é obrigatório!"}
+                placeholder="Nome"
+                icon={BsBuildings}
+              />
+              <Input
+                registerField={{
+                  ...register("quantPersonals", { required: true }),
+                }}
+                error={
+                  errors.quantPersonals &&
+                  "O número de personais é obrigatório!"
+                }
+                placeholder="Número de personais"
+                icon={PersonalIcon}
+              />
+              <Input
+                registerField={{ ...register("address", { required: true }) }}
+                error={errors.address && "O endereço é obrigatório!"}
+                placeholder="Endereço"
+                icon={AddressIcon}
+              />
+              <Input
+                registerField={{
+                  ...register("instagram", {
+                    pattern:
+                      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/i,
+                    message: "Instagram inválido!",
+                  }),
+                }}
+                error={errors.instagram && "Instagram inválido!"}
+                placeholder="Link do Instagram"
+                icon={BsInstagram}
+              />
+              <Input
+                registerField={{ ...register("contact", { required: true }) }}
+                error={errors.placeName && "O número de contato é obrigatório!"}
+                placeholder="Contato"
+                icon={ContactIcon}
+              />
               <Button
                 text="Localização"
                 icon={LocationIcon}
                 onClick={() => null}
               />
-            </form>
+            </div>
             <div className="flex flex-col pt-4 h-full justify-between">
               <h3 className="font-bold text-xl mb-2 ">Planos</h3>
               <div className="grid gap-2 mb-6 grid-flow-col max-w-[250px] overflow-x-auto grid-rows-2 pb-2">
@@ -72,6 +119,7 @@ const RegisterPlace = () => {
                         id="no-air"
                         name="airConditioned"
                         value="false"
+                        defaultChecked
                       />
                       <label htmlFor="no-air">Não</label>
                     </div>
@@ -95,6 +143,7 @@ const RegisterPlace = () => {
                         id="no-accessibility"
                         name="accessibility"
                         value="false"
+                        defaultChecked
                       />
                       <label htmlFor="no-accessibility">Não</label>
                     </div>
@@ -103,7 +152,9 @@ const RegisterPlace = () => {
               </div>
               <Button
                 outline
-                onClick={() => null}
+                onClick={handleSubmit(onSubmit, () =>
+                  setTimeout(clearErrors, 5000)
+                )}
                 text="Finalizar"
                 icon={BsCheck}
               />
