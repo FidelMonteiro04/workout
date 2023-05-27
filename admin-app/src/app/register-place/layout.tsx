1f9324c65947db076aad72763063c0e9579be136
Felipe Souza <felipesouza036@gmail.com>
2023-05-27 16:39:27 -0300

    Adding Image register context

--- admin-app/src/app/register-place/layout.tsx
+++ admin-app/src/app/register-place/layout.tsx
@@ -1,4 +1,18 @@
+"use client";
 import Header from "../components/Header";
+import { createContext, useState } from "react";
+
+export const ImageContext = createContext({});
+
+const ImageCtxProvider = ({ children }: { children: React.ReactNode }) => {
+  const [image, setImage] = useState<any>(null);
+
+  return (
+    <ImageContext.Provider value={{ image, setImage }}>
+      {children}
+    </ImageContext.Provider>
+  );
+};
 
 export default function RegisterLayout({
   children,
@@ -8,9 +22,11 @@ export default function RegisterLayout({
   return (
     <>
       <Header />
-      <section className="flex w-full min-h-full flex-col px-6">
-        {children}
-      </section>
+      <ImageCtxProvider>
+        <section className="flex w-full min-h-full flex-col px-6">
+          {children}
+        </section>
+      </ImageCtxProvider>
     </>
   );
 }
