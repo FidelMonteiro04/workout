b52b16943b2ec7a77faac4116655b92b432763eb
Felipe Souza <felipesouza036@gmail.com>
2023-05-28 14:45:13 -0300

    fix: Connection db link

--- backend/extensions/db.py
+++ backend/extensions/db.py
@@ -5,7 +5,7 @@ mongo = PyMongo()
 
 
 def init_app(app):
-    app.config["MONGO_URI"] = "mongodb+srv://fidelmonteiro05:UsjgHEdudQDygrnj@cluster0.vy4mzcd.mongodb.net/workout"
+    app.config["MONGO_URI"] = "mongodb+srv://fidelmonteiro05:UsjgHEdudQDygrnj@cluster0.vy4mzcd.mongodb.net/?retryWrites=true&w=majority"
     return mongo.init_app(app)
 
 
