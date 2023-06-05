930010c5d23d7e772c52d85bed69453be5076019
Fidel <Fidelmonteiro13@gmail.com>
2023-06-05 17:18:37 -0300

    feat: Update gymSchemas

--- backend/schemas/gym.py
+++ backend/schemas/gym.py
@@ -1,17 +1,37 @@
-from marshmallow import Schema, fields, validate
-from ..schemas.owner import OwnerSchema
-from ..schemas.plan import PlanSchema
-import uuid
+from schemas.owner import ownerSchema
 
-class GymSchema(Schema):
-    id = fields.UUID(default=uuid.uuid4, required=True)
-    cnpj = fields.String(required=True, validate=validate.Length(equal=14), unique=True)
-    name = fields.String(required=True, validate=validate.Length(min=3, max= 255))
-    owner = fields.ReferenceField(OwnerSchema, required=True, referencefield='id')
-    airConditioning = fields.Boolean(required=True)
-    minnorPlan = fields.Integer(required=True)
-    biggerPlan = fields.Integer(required=True)
-    plans = fields.List(fields.ReferenceField(PlanSchema, required=True, referencefield='id'))
-    acessibility = fields.Boolean(required=True)
-    personal = fields.Integer(required=True, min=1)
-    thumbnail = fields.String(required=True, max=255, unique=True)
+gymSchema = {
+    'type': 'object',
+    'required': ['cnpj', 'name', 'owner', 'airconditioner', 'personal', 'minorPlan', 'biggerPlan','acessilibility'],
+    'properties': {
+        'cnpj': {
+            'type': 'string',
+            'length': 14,
+            'unique': True
+            },
+        'name': {
+            'type': 'string'
+        },
+        'owner': {
+            '$ref': 'ownerSchema'
+        },
+        'airconditioner': {
+            'type': 'boolean'
+        },
+        'personal': {
+            'type': 'number'
+        },
+        'minorPlan': {
+            'type': 'number'
+        },
+        'biggerPlan': {
+            'type': 'number'
+        },
+        'acessilibility': {
+            'type': 'boolean'
+        },
+    },
+    'definitions': {
+        'ownerSchema': ownerSchema
+        }
+}
