dfcb5c4f731d703075da848862280874829e7afb
Felipe Souza <felipesouza036@gmail.com>
2023-06-07 10:59:40 -0300

    feat: Creating Login route

--- admin-app/src/app/layout.tsx
+++ admin-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import Image from "next/image";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={`${montserrat.className} min-h-full`}>{children}</body>
+      <body className={`${montserrat.className} min-h-full`}>
+        <header className="w-full flex items-center justify-center py-8 lg:hidden">
+          <Image src="./logo.svg" alt="Workout Logo" width={160} height={160} />
+        </header>
+        <section className="w-full grid grid-cols-1 p-4 lg:grid-cols-2 lg:self-center lg:justify-self-center lg:min-h-screen">
+          <div className="lg:flex lg:flex-col lg:items-center lg:justify-center ">
+            <Image
+              src="./partner.svg"
+              width={540}
+              height={540}
+              alt="Seja nosso parceiro!"
+              className="hidden h-auto lg:block mb-4"
+              priority
+            />
+            <div className="w-full lg:px-5">
+              <h2 className="text-4xl font-bold text-secondary-500  mb-3">
+                Seja nosso <span className="text-primary-500">parceiro!</span>
+              </h2>
+              <p className="font-medium text-gray-500 text-2xl max-w-[320px]">
+                Um jeito muito mais fácil de crescer seu negócio!
+              </p>
+            </div>
+          </div>
+          {children}
+        </section>
+      </body>
     </html>
   );
 }
--- admin-app/src/app/page.tsx
+++ admin-app/src/app/page.tsx
@@ -2,9 +2,11 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Image from "next/image";
+import Link from "next/link";
 
 import Input from "./components/Input";
 import Button from "./components/Button";
+import PropertySelector from "./components/PropertySelector";
 
 import { BiRename } from "react-icons/bi";
 import { MdAlternateEmail } from "react-icons/md";
@@ -13,13 +15,9 @@ import { MdPassword } from "react-icons/md";
 import { HiOutlineDocumentText } from "react-icons/hi";
 import { GiWeightLiftingUp as GymIcon } from "react-icons/gi";
 import { MdStoreMallDirectory as StoreIcon } from "react-icons/md";
-import PropertySelector from "./components/PropertySelector";
 
 export default function Home() {
   const [propertyType, setPropertyType] = useState("gym");
-  const [pageOption, setPageOption] = useState<"register" | "login">(
-    "register"
-  );
 
   const {
     register,
@@ -29,129 +27,84 @@ export default function Home() {
   } = useForm();
 
   const onRegister = (data: any) => console.log(data);
-  const onLogin = (data: any) => console.log(data);
 
   return (
-    <>
-      <header className="w-full flex items-center justify-center py-8 lg:hidden">
-        <Image src="./logo.svg" alt="Workout Logo" width={160} height={160} />
-      </header>
-      <section className="w-full grid grid-cols-1 p-4 lg:grid-cols-2 lg:self-center lg:justify-self-center lg:min-h-screen">
-        <div className="lg:flex lg:flex-col lg:items-center lg:justify-center ">
-          <Image
-            src="./partner.svg"
-            width={540}
-            height={540}
-            alt="Seja nosso parceiro!"
-            className="hidden lg:block mb-4"
+    <div className="lg:flex lg:flex-col lg:items-center lg:justify-center">
+      <Image
+        src="./logo.svg"
+        alt="Workout Logo"
+        width={160}
+        height={160}
+        className="hidden lg:block mb-2"
+      />
+      <form
+        onSubmit={handleSubmit(onRegister, () => setTimeout(clearErrors, 5000))}
+        className="p-4 max-w-[300px] mx-auto flex flex-col items-center gap-4 lg:py-2"
+      >
+        <Input
+          registerField={{ ...register("name", { required: true }) }}
+          error={errors.name && "Nome é obrigatório!"}
+          icon={BiRename}
+          placeholder="Nome"
+        />
+
+        <Input
+          registerField={{ ...register("email", { required: true }) }}
+          error={errors.email && "Email é obrigatório!"}
+          icon={MdAlternateEmail}
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          registerField={{ ...register("password", { required: true }) }}
+          error={errors.password && "Senha é obrigatória!"}
+          icon={FaLock}
+          password
+          visibility
+          placeholder="Senha"
+        />
+
+        <Input
+          registerField={{
+            ...register("confirmPassword", { required: true }),
+          }}
+          error={errors.confirmPassword && "Este campo é obrigatório!"}
+          icon={MdPassword}
+          password
+          placeholder="Confirmar senha"
+        />
+        <Input
+          registerField={{ ...register("cnpj", { required: true }) }}
+          error={errors.cnpj && "CNPJ é obrigatório!"}
+          icon={HiOutlineDocumentText}
+          placeholder="CNPJ"
+        />
+        <div className="flex gap-2 justify-between w-full mb-2">
+          <PropertySelector
+            selected={propertyType === "gym"}
+            onClick={() => setPropertyType("gym")}
+            icon={GymIcon}
+            text="Academia"
           />
-          <div className="w-full lg:px-5">
-            <h2 className="text-4xl font-bold text-secondary-500  mb-3">
-              Seja nosso <span className="text-primary-500">parceiro!</span>
-            </h2>
-            <p className="font-medium text-gray-500 text-2xl max-w-[300px]">
-              {pageOption === "register"
-                ? "Cadastre sua loja ou academia"
-                : "Coloque seus dados para acessar sua conta"}
-            </p>
-          </div>
+          <PropertySelector
+            selected={propertyType === "store"}
+            onClick={() => setPropertyType("store")}
+            icon={StoreIcon}
+            text="Loja"
+          />
         </div>
-        <div className="lg:flex lg:flex-col lg:items-center lg:justify-center">
-          <Image
-            src="./logo.svg"
-            alt="Workout Logo"
-            width={160}
-            height={160}
-            className="hidden lg:block mb-2"
-          />
-          <form
-            onSubmit={handleSubmit(
-              pageOption === "register" ? onRegister : onLogin,
-              () => setTimeout(clearErrors, 5000)
-            )}
-            className="p-4 max-w-[300px] mx-auto flex flex-col items-center gap-4 lg:py-2"
+
+        <Button text="Criar conta" onClick={() => null} />
+        <span className="font-light text-secondary-500 text-center">
+          Já é parceiro?{" "}
+          <Link
+            href={"/login"}
+            className="text-primary-500 cursor-pointer underline transition hover:text-primary-600"
           >
-            {pageOption === "register" && (
-              <Input
-                registerField={{ ...register("name", { required: true }) }}
-                error={errors.name && "Nome é obrigatório!"}
-                icon={BiRename}
-                placeholder="Nome"
-              />
-            )}
-            <Input
-              registerField={{ ...register("email", { required: true }) }}
-              error={errors.email && "Email é obrigatório!"}
-              icon={MdAlternateEmail}
-              placeholder="Email"
-              type="email"
-            />
-            <Input
-              registerField={{ ...register("password", { required: true }) }}
-              error={errors.password && "Senha é obrigatória!"}
-              icon={FaLock}
-              password
-              visibility
-              forget={pageOption === "login"}
-              placeholder="Senha"
-            />
-            {pageOption === "register" && (
-              <>
-                <Input
-                  registerField={{
-                    ...register("confirmPassword", { required: true }),
-                  }}
-                  error={errors.confirmPassword && "Este campo é obrigatório!"}
-                  icon={MdPassword}
-                  password
-                  placeholder="Confirmar senha"
-                />
-                <Input
-                  registerField={{ ...register("cnpj", { required: true }) }}
-                  error={errors.cnpj && "CNPJ é obrigatório!"}
-                  icon={HiOutlineDocumentText}
-                  placeholder="CNPJ"
-                />
-
-                <div className="flex gap-2 justify-between w-full mb-2">
-                  <PropertySelector
-                    selected={propertyType === "gym"}
-                    onClick={() => setPropertyType("gym")}
-                    icon={GymIcon}
-                    text="Academia"
-                  />
-                  <PropertySelector
-                    selected={propertyType === "store"}
-                    onClick={() => setPropertyType("store")}
-                    icon={StoreIcon}
-                    text="Loja"
-                  />
-                </div>
-              </>
-            )}
-
-            <Button
-              text={pageOption === "register" ? "Criar conta" : "Entrar"}
-              onClick={() => null}
-            />
-            <span className="font-light text-secondary-500 text-center">
-              {pageOption === "register"
-                ? "Já é parceiro?"
-                : "Ainda não é nosso parceiro?"}{" "}
-              <span
-                onClick={() =>
-                  setPageOption((prev) =>
-                    prev === "register" ? "login" : "register"
-                  )
-                }
-                className="text-primary-500 cursor-pointer underline transition hover:text-primary-600"
-              >
-                Clique aqui!
-              </span>
-            </span>
-          </form>
-        </div>
-      </section>
-    </>
+            Clique aqui!
+          </Link>
+        </span>
+      </form>
+    </div>
   );
 }
