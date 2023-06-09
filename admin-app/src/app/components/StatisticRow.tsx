008ad1030c7604dc7985e2c736984423ca1e230a
Felipe Souza <felipesouza036@gmail.com>
2023-06-09 15:09:46 -0300

    feat: Statistic Row component

---
+++ admin-app/src/app/components/StatisticRow.tsx
@@ -0,0 +1,25 @@
+import { IconType } from "react-icons";
+
+interface Props {
+  icon: IconType;
+  statistics: Array<{ title: string; value: string }>;
+}
+
+const StatisticRow = ({ icon: Icon, statistics }: Props) => {
+  return (
+    <div className="flex justify-between lg:justify-center pb-2 mb-4 lg:mb-0 gap-2 w-full whitespace-nowrap overflow-x-auto items-center">
+      <Icon size={20} className=" text-zinc-500 mr-1 shrink-0" />
+      {statistics.map(({ title, value }, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center justify-center text-sm mr-4"
+        >
+          <span className="font-semibold text-zinc-500">{title}</span>
+          <span className="font-semibold text-primary-500">{value}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default StatisticRow;
