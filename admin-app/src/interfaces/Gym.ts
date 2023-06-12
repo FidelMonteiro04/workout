02ed40ce45003f56e19050973e9d8208b2d400d9
Felipe Souza <felipesouza036@gmail.com>
2023-06-12 09:18:41 -0300

    feat: Gym and Store interfaces

---
+++ admin-app/src/interfaces/Gym.ts
@@ -0,0 +1,14 @@
+export interface Gym {
+  accessibility: boolean;
+  address: string;
+  airConditioner: boolean;
+  contact: string;
+  image: string;
+  instagram?: string;
+  lat: string;
+  lng: string;
+  name: string;
+  owner_id: string;
+  personals: number;
+  _id: string;
+}
---
+++ admin-app/src/interfaces/Store.ts
@@ -0,0 +1,11 @@
+export interface Store {
+  _id: string;
+  owner_id: string;
+  image: string;
+  name: string;
+  address: string;
+  lat: string;
+  lng: string;
+  contact: string;
+  instagram?: string;
+}
